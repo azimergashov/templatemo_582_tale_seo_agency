df47a549c38d647f6de8113322f58d3a1e09f4d6
azimergashov <azimboy.ergashov.1@gmail.com>
2022-11-16 17:36:36 +0500

    asdas

---
+++ telegramSend.js
@@ -0,0 +1,52 @@
+
+const  telegram_bot_id = "5570784931:AAHAk5u0j9bqSIYGP3L1GjOsTUylRwnDDH8"
+
+const chat_id = -1001502748774
+
+const telInput = document.querySelector('.number-input')
+
+const telInputChange = function () {
+    telInput.value = '+998 '
+}
+var name, surname, address, tel_number, u_message, message
+var getValues = function () {
+
+    u_name = document.getElementById('user_name')
+    surname = document.getElementById('surname')
+    address = document.getElementById('address')
+    tel_number = document.getElementById('tel_number')
+    u_message = document.getElementById('message')
+    message = "Ismi: " + u_name + "\nFamiliya: " + surname + "\nManzil: " + address + '\ntel-raqami:' + tel_number, '\nIzoh:' + u_message;
+}
+
+
+
+var sendTelegram = function () {
+    getValues()
+    const  settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    }
+
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("tel_number").value = "";
+    document.getElementById("message").value = "";
+    alert("Tezzda alloqaga chiqamiz")
+    return false;
+
+}
